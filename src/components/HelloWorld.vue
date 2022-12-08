9b242ba77788fef56f97a414898ef45f7cf6235e
kosukej <81564943+kosukej@users.noreply.github.com>
2022-12-08 21:52:03 +0900

    🎉Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> これをいじることでホットリロードが実現されます</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Test from './App.vue'
+import './css/index.css'
+
+createApp(Test).mount('#app')
